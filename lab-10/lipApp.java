1d5337b9903d48eb500e8a0cd49ec097ade0840b
namuunaa <128396843+namuunaaa@users.noreply.github.com>
2025-05-15 12:53:15 +0800

    Add files via upload

---
+++ lab-10/lipApp.java
@@ -0,0 +1,12 @@
+public class lipApp {
+	public static void main(String[] args) {
+		Library lib = new Library();
+		lib.addBook("Gedes", 1921);
+		lib.addBook("1984", 1949);
+		lib.addBook("Suun Zam", 2020);
+		for (Book b : lib.getRecentBooks(2000)) {
+			System.out.println(b.getTitle() + ": " + b.getYear());
+		}
+	}
+}
+    
